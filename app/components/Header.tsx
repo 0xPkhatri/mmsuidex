d74f041662b5c9652f346b707ab594fda126d69c
Prakash Khatri <pkhatri981@gmail.com>
2026-02-06 15:07:26 +0530

    header

---
+++ app/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <h1>Sui Order Book Market Maker</h1>
+      <p>Automated limit-order liquidity provision</p>
+    </header>
+  );
+}
